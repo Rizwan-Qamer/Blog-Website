61e536cb6db8c11f6d4f254a7099492b801d47f4
Rizwan Qamer <rizwanqamer321@gmail.com>
2024-11-18 21:45:56 +0500

    Connect Database

--- myblog1/models.py
+++ myblog1/models.py
@@ -8,7 +8,7 @@ class Blog(models.Model):
     # id = models.AutoField(primary_key=True)
 
     def __str__(self):
-        return self.title
+        return self.Blog
 
     class Meta:
         managed = True  # Ensure Django manages this table (optional)
